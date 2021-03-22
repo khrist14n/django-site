d4c9e8592032c696b844ee21a4286198d34ab860
vagrant <vagrant@vagrant.vm>
2021-03-22 04:28:32 +0000

    app

--- djone/gettingstarted/settings.py
+++ djone/gettingstarted/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "jkkhohoihihogh3otu34ut827t892y349ghgh924h99y89y98"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "hello",
+    "home",
 ]
 
 MIDDLEWARE = [
